029ee579300784a82d569b0f54e8a4998a9ef7f2
liuchenxu-uppsala <chenxu.l@outlook.com>
2026-06-14 12:35:21 +0200

    update icon2

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body style={{ fontFamily: 'Inter, system-ui, sans-serif' }}>
         <LangProvider>
